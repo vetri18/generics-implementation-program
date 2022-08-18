7b830a3ab7c4f9ceaeea2aeeb69dc9c5089f72b5
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-18 15:17:38 +0530

    refractor 2

---
+++ MaxGenericRefactor2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaximumProblem
+{
+    public class MaxGenericRefactor2<T> where T : IComparable
+    {
+        public T first_Value, second_Value, third_Value;
+        public MaxGenericRefactor2(T first_Value, T second_Value, T third_Value)
+        {
+            this.first_Value = first_Value;
+            this.second_Value = second_Value;
+            this.third_Value = third_Value;
+        }
+
+        public void TestMaximum(T first_Value, T second_Value, T third_Value)
+        {
+            if (first_Value.CompareTo(second_Value) >= 0 && first_Value.CompareTo(third_Value) >= 0)
+                Console.WriteLine("Greatest Value is = " + first_Value);
+            else if (second_Value.CompareTo(first_Value) >= 0 && second_Value.CompareTo(third_Value) >= 0)
+                Console.WriteLine("Greatest Value is = " + second_Value);
+            else if (third_Value.CompareTo(second_Value) >= 0 && third_Value.CompareTo(first_Value) >= 0)
+                Console.WriteLine("Greatest Value is = " + third_Value);
+            else
+                Console.WriteLine("Greatest Word is = " + first_Value);
+        }
+    }
+}
---
+++ Program2.cs
@@ -0,0 +1,42 @@
+﻿namespace GenericsMaximumProblem
+{
+    class ProgramRefactor2
+    {
+        public static void Main()
+        {
+            MaxGenericRefactor2<int> maxInt;
+            MaxGenericRefactor2<float> maxFloat;
+            MaxGenericRefactor2<string> maxString;
+
+            //Calling Generic Method for Integer value
+            Console.Write("Enter First Integer Value = ");
+            int first_Int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Integer  Value = ");
+            int second_Int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Integer Value = ");
+            int third_Int = Convert.ToInt32(Console.ReadLine());
+            maxInt = new MaxGenericRefactor2<int>(first_Int, second_Int, third_Int);
+            maxInt.TestMaximum(first_Int, second_Int, third_Int);
+
+            //Calling Generic Method for Float Value
+            Console.Write("Enter First Decimal Value = ");
+            float first_Float = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Decimal Value = ");
+            float second_Float = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Decimal Value = ");
+            float third_Float = Convert.ToInt32(Console.ReadLine());
+            maxFloat = new MaxGenericRefactor2<float>(first_Float, second_Float, third_Float);
+            maxFloat.TestMaximum(first_Float, second_Float, third_Float);
+
+            //Calling Generic Method for String Value
+            Console.Write("Enter First Word = ");
+            string first_Value = Console.ReadLine();
+            Console.Write("Enter Second Word = ");
+            string second_Value = Console.ReadLine();
+            Console.Write("Enter Third Word = ");
+            string third_Value = Console.ReadLine();
+            maxString = new MaxGenericRefactor2<string>(first_Value, second_Value, third_Value);
+            maxString.TestMaximum(first_Value, second_Value, third_Value);
+        }
+    }
+}
