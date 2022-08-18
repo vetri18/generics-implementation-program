745cb7810b4e596baa4795d90c946db18e773d78
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-18 15:19:32 +0530

    max method to print the max uc5

---
+++ PrintMaximumUC5.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaximumProblem
+{
+    public class PrintMaximumUC5
+    {
+        public void TestMaximum<T>(T first_Value, T second_Value, T third_Value) where T : IComparable
+        {
+            if (first_Value.CompareTo(second_Value) >= 0 && first_Value.CompareTo(third_Value) >= 0)
+                PrintMax<T>(first_Value);
+            else if (second_Value.CompareTo(first_Value) >= 0 && second_Value.CompareTo(third_Value) >= 0)
+                PrintMax<T>(second_Value);
+            else if (third_Value.CompareTo(second_Value) >= 0 && third_Value.CompareTo(first_Value) >= 0)
+                PrintMax<T>(second_Value);
+            else
+                Console.WriteLine("Greatest Word is = " + first_Value);
+        }
+        public void PrintMax<T>(T max)
+        {
+            Console.WriteLine("Greatest Value is = " + max);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace EmployeeWage
+﻿namespace GenericsMaximumProblem
 {
-    class Program
+    class ProgramUC5
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Welcome to Maximum Problem Using Generics");
+            PrintMaximumUC5 printMaximumUC5 = new PrintMaximumUC5();
+
+            //Calling Generic Method for Integer value
+            Console.Write("Enter First Integer Value = ");
+            int first_Int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Integer  Value = ");
+            int second_Int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Integer Value = ");
+            int third_Int = Convert.ToInt32(Console.ReadLine());
+            printMaximumUC5.TestMaximum(first_Int, second_Int, third_Int);
+
+            //Calling Generic Method for Float Value
+            Console.Write("Enter First Decimal Value = ");
+            float first_Float = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Decimal Value = ");
+            float second_Float = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Decimal Value = ");
+            float third_Float = Convert.ToInt32(Console.ReadLine());
+            printMaximumUC5.TestMaximum(first_Float, second_Float, third_Float);
+
+            //Calling Generic Method for String Value
+            Console.Write("Enter First Word = ");
+            string first_Value = Console.ReadLine();
+            Console.Write("Enter Second Word = ");
+            string second_Value = Console.ReadLine();
+            Console.Write("Enter Third Word = ");
+            string third_Value = Console.ReadLine();
+            printMaximumUC5.TestMaximum(first_Value, second_Value, third_Value);
         }
     }
 }
