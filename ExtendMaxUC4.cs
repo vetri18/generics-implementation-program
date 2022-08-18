80b97d69a1493b9013b609bcd82fe07986f5d69f
vetri18 <109862568+vetri18@users.noreply.github.com>
2022-08-18 15:18:45 +0530

    max method to take more then three parameters uc 4

---
+++ ExtendMaxUC4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsMaximumProblem
+{
+    public class ExtendMaxUC4
+    {
+        public void TestMaximum<T>(T first_Value, T second_Value, T third_Value, T fourth_Value) where T : IComparable
+        {
+            if (first_Value.CompareTo(second_Value) >= 0 && first_Value.CompareTo(third_Value) >= 0 && first_Value.CompareTo(fourth_Value) >= 0)
+                Console.WriteLine("Greatest Value is = " + first_Value);
+            else if (second_Value.CompareTo(first_Value) >= 0 && second_Value.CompareTo(third_Value) >= 0 && second_Value.CompareTo(fourth_Value) >= 0)
+                Console.WriteLine("Greatest Value is = " + second_Value);
+            else if (third_Value.CompareTo(second_Value) >= 0 && third_Value.CompareTo(first_Value) >= 0 && third_Value.CompareTo(fourth_Value) >= 0)
+                Console.WriteLine("Greatest Value is = " + third_Value);
+            else if (fourth_Value.CompareTo(second_Value) >= 0 && fourth_Value.CompareTo(first_Value) >= 0 && fourth_Value.CompareTo(third_Value) >= 0)
+                Console.WriteLine("Greatest Value is = " + fourth_Value);
+            else
+                Console.WriteLine("Greatest Word is = " + first_Value);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace EmployeeWage
+﻿namespace GenericsMaximumProblem
 {
-    class Program
+    class ProgramUC4
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
-            Console.WriteLine("Welcome to Maximum Problem Using Generics");
+            ExtendMaxUC4 extendMaxUC4 = new ExtendMaxUC4();
+
+            //Calling Generic Method for Integer value
+            Console.Write("Enter First Integer Value = ");
+            int first_Int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Integer  Value = ");
+            int second_Int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Integer Value = ");
+            int third_Int = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Fourth Integer Value = ");
+            int fourth_Int = Convert.ToInt32(Console.ReadLine());
+            extendMaxUC4.TestMaximum<int>(first_Int, second_Int, third_Int, fourth_Int);
+
+            //Calling Generic Method for Float Value
+            Console.Write("Enter First Decimal Value = ");
+            float first_Float = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Second Decimal Value = ");
+            float second_Float = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Third Decimal Value = ");
+            float third_Float = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Fourth Decimal Value = ");
+            float fourth_Float = Convert.ToInt32(Console.ReadLine());
+            extendMaxUC4.TestMaximum<float>(first_Float, second_Float, third_Float, fourth_Float);
+
+            //Calling Generic Method for String Value
+            Console.Write("Enter First Word = ");
+            string first_Value = Console.ReadLine();
+            Console.Write("Enter Second Word = ");
+            string second_Value = Console.ReadLine();
+            Console.Write("Enter Third Word = ");
+            string third_Value = Console.ReadLine();
+            Console.Write("Enter Fourth Word = ");
+            string fourth_Value = Console.ReadLine();
+            extendMaxUC4.TestMaximum<string>(first_Value, second_Value, third_Value, fourth_Value);
         }
     }
 }
