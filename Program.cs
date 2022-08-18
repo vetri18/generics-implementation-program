20dba6b8af22991b9655f355d3e4037ac90dd6d8
vetrivel <vetrivel.kce@gmail.com>
2022-08-18 13:21:15 +0530

    welcome to generic implementation problems

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Maximum Problem Using Generics");
+        }
+    }
+}
